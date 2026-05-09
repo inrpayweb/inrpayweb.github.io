b79ab0717c23be0410cbb74a318aecd67ec7ef0e
inrpayweb <inrpaysupport@gmail.com>
2026-05-09 22:02:51 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'inrpay-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
